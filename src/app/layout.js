806f8700cc1b84f1401a01b9db8c3846b8e64b25
thanhtoan138 <vangthanhtoan1995@gmail.com>
2024-06-11 17:41:26 +0700

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google";
-import "./globals.css";
-
+import Footer from "./main/footer/page";
+import Header from "./main/header/page";
+import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -11,7 +13,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
---
+++ src/app/main/header/page.js
@@ -0,0 +1,229 @@
+'use client';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import { useState } from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+// import React, { Component } from 'react';
+import '../../../scss/header.scss';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
+// import { Link } from "react-router-dom";
+
+const Header = () => {
+
+    const [show, setShow] = useState(false);
+    const [showRegister, setShowRegister] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleCloseRegister = () => setShowRegister(false);
+    const handleShow = () => setShow(true);
+    const handleShowRegister = () => setShowRegister(true);
+    const pathname = usePathname()
+    return (
+
+        <div className='header w-100'>
+            <Navbar className="bg-body-tertiary">
+                <Container>
+                    <Navbar.Brand href="#home">
+                        <img src='https://www.mioto.vn/static/media/logo-full.ea382559.png' alt='' width={200} />
+                    </Navbar.Brand>
+                    <Navbar.Toggle />
+                    <Navbar.Collapse className="justify-content-end">
+                        <Navbar.Text className='ntext'>
+                            Về Mioto
+                        </Navbar.Text>
+                        <Navbar.Text className='ntext'>
+                            Trở thành chủ xe
+                        </Navbar.Text>
+                        <Navbar.Text className='ntext'>
+                            <span>|</span>
+                        </Navbar.Text>
+                        <Navbar.Text className='ntext'>
+                            <a onClick={handleShowRegister}>Đăng ký</a>
+                        </Navbar.Text>
+                        <Navbar.Text >
+                            <a className='dn btn' onClick={handleShow}>Đăng nhập</a>
+                        </Navbar.Text>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            <Container>
+                <div className='nav-head'>
+                    <div className='img-bg w-100 mt-3'>
+                        <img src='https://www.mioto.vn/static/media/bg-landingpage-1.34e13e49.png' alt='' width={1300} height={600} />
+                    </div>
+                    <div className='title'>
+                        <p className='first'>Mioto - Cùng Bạn<br /> Đến Mọi Hành Trình</p>
+                        <hr className='second' noshade="noshade" />
+                        <p className='three'>Trải nghiệm sự khác biệt từ <span>hơn 8000</span> xe gia đình đời mới khắp Việt Nam</p>
+                    </div>
+                    <div className='nav-pill'>
+                        <Nav variant="pills" defaultActiveKey="/main3">
+                            <Nav.Item className='nav-pill-c' style={{ borderRadius: '10px 0px 0px 0px' }}>
+                                <Nav.Link eventKey="link-1" style={{ borderRadius: "0" }}>
+                                    <Link href='/main3' scroll={false} style={{ textDecoration: 'none', color: 'white' }}>
+                                        <i class="bi bi-bus-front-fill"></i>
+                                        &nbsp;Xe tự lái
+                                    </Link>
+
+                                </Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item className='nav-pill-c'>
+                                <Nav.Link eventKey="link-2" style={{ borderRadius: "0" }}>
+                                    <Link href='/main2' scroll={false} style={{ textDecoration: 'none', color: 'white' }}>
+                                        <i class="bi bi-bus-front-fill"></i>
+                                        &nbsp;Xe có tài xế
+                                    </Link>
+
+                                </Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item className='nav-pill-c' style={{ borderRadius: '0px 10px 0px 0px' }}>
+                                <Nav.Link eventKey="link-3" style={{ borderRadius: "0" }}>
+                                    <Link href='/main2' scroll={false} style={{ textDecoration: 'none', color: 'white' }}>
+                                        <i class="bi bi-calendar-date"></i>
+                                        &nbsp;Thuê xe dài hạn
+                                    </Link>
+
+                                </Nav.Link>
+                            </Nav.Item>
+                            &nbsp;
+                        </Nav>
+                    </div>
+                </div>
+            </Container>
+
+
+            <Modal
+                show={show}
+                onHide={handleClose}
+                backdrop="static"
+                keyboard={false}
+            >
+
+                <a variant="secondary" onClick={handleClose} style={{
+                    marginLeft: '430px', marginRight: '28px', fontSize: '15px', borderRadius: '100%',
+                    paddingLeft: '17px', paddingTop: '10px', paddingBottom: '7px', backgroundColor: 'rgb(231, 231, 229)', fontWeight: '500'
+                }}>
+                    X
+                </a>
+
+                <Modal.Body>
+                    <h4 style={{ textAlign: 'center', marginBottom: '25px' }}>Đăng nhập</h4>
+                    <div className='form'>
+                        <Form.Label htmlFor="sdt">Số điện thoại hoặc email</Form.Label>
+                        <Form.Control
+                            type="text"
+                            id="sdt"
+                            aria-describedby="passwordHelpBlock"
+                        />
+
+                        <Form.Label htmlFor="inputPassword5" style={{ marginTop: '10px' }}>Mật khẩu </Form.Label>
+                        <Form.Control
+                            type="password"
+                            id="inputPassword5"
+                            aria-describedby="passwordHelpBlock"
+
+                        />
+                        <i class="bi bi-eye-slash" style={{ position: 'relative', bottom: '30px', left: '437px' }}></i>
+
+                        <p style={{ marginLeft: '330px', marginTop: '10px', color: 'rgb(95, 207, 134)' }}>Quên mật khẩu?</p>
+                        <a className='dn btn '>Đăng nhập</a>
+                        <p style={{ textAlign: 'center', paddingTop: '10px' }}>Bạn chưa là thành viên? <span style={{ color: 'rgb(95, 207, 134)', fontWeight: "500" }}>Đăng ký ngay</span></p>
+                        <p className='equal'>
+                            <a className='fb btn'><img src='https://nhanhtravel.com/wp-content/uploads/2022/12/TS-FB-Icon1-e1670787566310.png' width={18} /> Facebook</a>
+                            <a className='gg btn'><img src='https://cdn.pixabay.com/photo/2021/05/24/09/15/google-logo-6278331_960_720.png' width={18} /> Google</a>
+                        </p>
+
+                    </div>
+                </Modal.Body>
+
+            </Modal>
+
+            {/* regíster */}
+
+            <Modal
+                show={showRegister}
+                onHide={handleClose}
+                backdrop="static"
+                keyboard={false}
+            >
+
+                <a variant="secondary" onClick={handleCloseRegister} style={{
+                    marginLeft: '430px', marginRight: '28px', fontSize: '15px', borderRadius: '100%',
+                    marginTop: '27px', paddingLeft: '17px', paddingTop: '10px', paddingBottom: '7px', backgroundColor: 'rgb(231, 231, 229)', fontWeight: '500'
+                }}>
+                    X
+                </a>
+
+                <Modal.Body style={{ height: '700px' }}>
+                    <h4 style={{ textAlign: 'center', marginBottom: '25px', }}>Đăng ký</h4>
+                    <div className='form2 '>
+                        <Form.Label htmlFor="sdt">Số điện thoại</Form.Label>
+                        <Form.Control
+                            type="text"
+                            id="sdt"
+                            aria-describedby="passwordHelpBlock"
+                        />
+
+                        <Form.Label htmlFor="sdt">Tên hiển thị</Form.Label>
+                        <Form.Control
+                            type="text"
+                            id="sdt"
+                            aria-describedby="passwordHelpBlock"
+                        />
+
+                        <Form.Label htmlFor="inputPassword5" style={{ marginTop: '10px' }}>Mật khẩu </Form.Label>
+                        <Form.Control
+                            type="password"
+                            id="inputPassword5"
+                            aria-describedby="passwordHelpBlock"
+
+                        />
+
+                        <Form.Label htmlFor="inputPassword5" style={{ marginTop: '10px' }}>Xác nhận mật khẩu</Form.Label>
+                        <Form.Control
+                            type="password"
+                            id="inputPassword5"
+                            aria-describedby="passwordHelpBlock"
+
+                        />
+
+                        <Form.Label htmlFor="sdt">Mã giới thiệu</Form.Label>
+                        <Form.Control
+                            type="text"
+                            id="inputPassword5"
+                            aria-describedby="passwordHelpBlock"
+                        />
+                        <Form style={{ display: 'flex', marginTop: '10px' }}>
+                            {['checkbox'].map((type) => (
+                                <div key={`default-${type}`} className="mb-3">
+                                    <Form.Check // prettier-ignore
+                                        type={type}
+                                        id={`default-${type}`}
+                                    />
+                                </div>
+                            ))}
+                            &nbsp;<p> Tối đã đọc và đồng ký với <span style={{ fontWeight: '500', color: '#5fcf86' }}>Chính sách và quy định </span>
+                                và <span style={{ fontWeight: '500', color: '#5fcf86' }}>Chính sách bảo vệ dữ liệu cá nhân</span> của Mioto
+                            </p>
+                        </Form>
+                        <i class="a bi bi-eye-slash" style={{ position: 'relative', bottom: '175px', left: '437px' }}></i>
+                        <i class="b bi bi-eye-slash" style={{ position: 'relative', bottom: '105px', left: '422px' }}></i>
+                        <a className='dk btn'>Đăng ký</a>
+                        <p className='equal'>
+                            <a className='fb btn'><img src='https://nhanhtravel.com/wp-content/uploads/2022/12/TS-FB-Icon1-e1670787566310.png' width={18} /> Facebook</a>
+                            <a className='gg btn'><img src='https://cdn.pixabay.com/photo/2021/05/24/09/15/google-logo-6278331_960_720.png' width={18} /> Google</a>
+                        </p>
+
+                    </div>
+                </Modal.Body>
+
+            </Modal>
+
+        </div>
+
+
+    )
+}
+export default Header;
