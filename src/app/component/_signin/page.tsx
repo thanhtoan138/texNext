4756e33eb9ff13887829bfde1c117f16ed170168
toanvt138 <toanvt138@gmail.com>
2024-06-25 22:01:00 +0700

    25/6

--- src/app/component/_signin/page.tsx
+++ src/app/component/_signin/page.tsx
@@ -1,30 +1,27 @@
 
+import { trace } from 'console';
 import React, { useState, FormEvent } from 'react'
 
 export default function Page() {
   const [isLoading, setIsLoading] = useState<boolean>(false)
-
+  const [phone,setPhone] = useState<any>('');
+  const [password,setPassword] = useState<string>('');
   async function onSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault()
     setIsLoading(true) // Set loading to true when the request starts
 
     try {
-      const formData = new FormData(event.currentTarget)
-      const response = await fetch(`http://localhost:8000/users`, {
-        method: 'POST',
-        body: JSON.stringify(formData)
-      })
-
-      // Handle response if necessary
+    
+      const response = await fetch('http://localhost:8000/users');
       const data = await response.json();
-      if (data.phone === formData) {
-        console.log('sai')
-      } else {
-        console.log('ok')
-      }
-
-
-      // ...
+      // Handle response if necessary
+      data?.map((u)=>{
+         if(u.phone === phone && +u.password === +password){
+              alert('ok to');
+            }else{
+              alert('failed  to');
+            }
+      })
     } catch (error) {
       // Handle error if necessary
       console.error(error)
@@ -35,8 +32,8 @@ export default function Page() {
 
   return (
     <form onSubmit={onSubmit}>
-      <input type="text" name="phone" />
-      <input type="text" name="password" />
+      <input type="text" name="phone"  onChange={(e)=>setPhone(e.target.value)}/>
+      <input type="text" name="password" onChange={(e)=>setPassword(e.target.value)}/>
       <button type="submit" disabled={isLoading}>
         {isLoading ? 'Loading...' : 'Submit'}
       </button>
